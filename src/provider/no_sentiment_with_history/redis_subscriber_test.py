9e5c6d9f54bc560cc8855059127dfea9c7c5c838
krist-tm-vagrant <krist.jin.apply@gmail.com>
2015-12-05 19:16:05 -0500

    add time window to redis_subscriber_test.py

--- src/provider/no_sentiment_with_history/redis_subscriber_test.py
+++ src/provider/no_sentiment_with_history/redis_subscriber_test.py
@@ -1,12 +1,16 @@
 import redis
 import pickle
-from collections import Counter
+import time
+from collections import Counter, deque
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 pubsub = redis.pubsub()
 pubsub.subscribe('word_stats')
 message = pubsub.listen()
-counter = Counter()
+result = Counter()
+active_counters = deque()
+
+TIME_WINDOW = 1 * 60 * 60
 
 while True:
     pickled_stats = message.next().get('data')
@@ -16,5 +20,10 @@ while True:
         stats = pickle.loads(pickled_stats)
     except TypeError, e:
         continue
-    counter += Counter(dict(stats))  # list to dict then to Counter then add
-    print counter.most_common(20)
+    this_counter = Counter(dict(stats))
+    current_time = int(time.time())
+    active_counters.append((this_counter, current_time))
+    result += this_counter  # add current counter to the result
+    while current_time - active_counters[0][1] > TIME_WINDOW:  # if old counter expire
+        result -= active_counters.popleft()[0]  # minus old counter
+    print result.most_common(20)
